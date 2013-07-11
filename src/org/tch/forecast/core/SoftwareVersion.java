9b9e7f811ba339ea153bdca57c9a00906779101a
Nathan <unknown>
2013-07-11 14:21:06 +0000

    Finished next version

--- src/org/tch/forecast/core/SoftwareVersion.java
+++ src/org/tch/forecast/core/SoftwareVersion.java
@@ -2,8 +2,8 @@ package org.tch.forecast.core;
 
 public class SoftwareVersion {
   public static final String VERSION_MAJOR = "2";
-  public static final String VERSION_MINOR = "14";
-  public static final String VERSION_RELEASE = "20130708";
+  public static final String VERSION_MINOR = "15";
+  public static final String VERSION_RELEASE = "20130711";
   
   public static final String VERSION = VERSION_MAJOR + "." + VERSION_MINOR; 
   
