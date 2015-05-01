a4291069aa9f422dd6f932992f7ff1d9afce252a
Nathan <unknown>
2015-05-01 05:43:49 +0000

    Added new CVX codes for 3.11.05

--- src/org/tch/forecast/core/SoftwareVersion.java
+++ src/org/tch/forecast/core/SoftwareVersion.java
@@ -8,8 +8,8 @@ public class SoftwareVersion
   // javap -constants -classpath tch-forecaster-test.jar org.tch.forecast.core.SoftwareVersion
   public static final String VERSION_MAJOR = "3";
   public static final String VERSION_MINOR = "11";
-  public static final String VERSION_PATCH = "04";
-  public static final String VERSION_RELEASE = "20150213";
+  public static final String VERSION_PATCH = "05";
+  public static final String VERSION_RELEASE = "20150501";
 
   public static final String VERSION = VERSION_MAJOR + "." + VERSION_MINOR + "." + VERSION_PATCH;
 
