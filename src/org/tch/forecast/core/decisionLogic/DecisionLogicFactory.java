5f8ce0a810be051d187c0aae966491d590b827d0
GordonC <unknown>
2015-07-14 21:35:36 +0000

    Updated Pneumo65 and Influenza for 2015

--- src/org/tch/forecast/core/decisionLogic/DecisionLogicFactory.java
+++ src/org/tch/forecast/core/decisionLogic/DecisionLogicFactory.java
@@ -3,12 +3,17 @@ package org.tch.forecast.core.decisionLogic;
 public class DecisionLogicFactory
 {
   public static final String DL_FLU_2014 = "DL FLU 2014";
+  public static final String DL_FLU_2015 = "DL FLU 2015";
   public static DecisionLogic getDecisionLogic(String name)
   {
     DecisionLogic dl = null;
     if (name.equalsIgnoreCase(DL_FLU_2014))
     {
       return new Flu2014DecisionLogic();
+    }
+    else if (name.equalsIgnoreCase(DL_FLU_2015))
+    {
+      return new Flu2015DecisionLogic();
     }
     return dl;
   }
