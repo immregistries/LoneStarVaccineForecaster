88d5a9ce903d4218a1f8e18a2d9b452ef67da117
GordonC <unknown>
2016-01-29 22:25:01 +0000

    Added new Decision Logic modules for the two different MeningococcalB states (MeningTrumenba and MeningBexsero are deprecated)

--- src/org/tch/forecast/core/decisionLogic/DecisionLogicFactory.java
+++ src/org/tch/forecast/core/decisionLogic/DecisionLogicFactory.java
@@ -4,6 +4,9 @@ public class DecisionLogicFactory
 {
   public static final String DL_FLU_2014 = "DL FLU 2014";
   public static final String DL_FLU_2015 = "DL FLU 2015";
+  public static final String DL_MENB_TWO_DOSE = "DL MenB TwoDose";
+  public static final String DL_MENB_MULTI_DOSE = "DL MenB MultiDose";
+  
   public static DecisionLogic getDecisionLogic(String name)
   {
     DecisionLogic dl = null;
@@ -14,6 +17,14 @@ public class DecisionLogicFactory
     else if (name.equalsIgnoreCase(DL_FLU_2015))
     {
       return new Flu2015DecisionLogic();
+    }
+    else if (name.equalsIgnoreCase(DL_MENB_TWO_DOSE))
+    {
+      return new MenBTwoDoseStateDecisionLogic();
+    }
+    else if (name.equalsIgnoreCase(DL_MENB_MULTI_DOSE))
+    {
+      return new MenBMultiDoseStateDecisionLogic();
     }
     return dl;
   }
---
+++ src/org/tch/forecast/core/decisionLogic/MenBMultiDoseStateDecisionLogic.java
@@ -0,0 +1,48 @@
+package org.tch.forecast.core.decisionLogic;
+
+import java.util.List;
+
+import org.tch.forecast.core.VaccinationDoseDataBean;
+import org.tch.forecast.core.logic.DataStore;
+
+public class MenBMultiDoseStateDecisionLogic extends MenBDecisionLogic {
+
+  /**
+   * Determines which schedule to transition to next.
+   * 
+   * This method is only entered if there are both Bexsero and Trumenba doses present.
+   */
+  @Override
+  public String getTransition(DataStore ds) {
+    init(ds);
+    ds.log("Start DL MenB Decision Logic for Multiple Dose State");
+//    final String secondDoseNeeded = getTransitionValue(TRANSITION_SECOND_DOSE_NEEDED);
+//    final String finalDoseNeeded = getTransitionValue(TRANSITION_FINAL_DOSE_NEEDED);
+//    final String noMoreDosesNeeded = getTransitionValue(TRANSITION_NO_MORE_DOSES_NEEDED);
+//    final String nextBexseroDoseNeeded = "B2";//getConstantValue(VALID_VACCINE);
+
+    List<VaccinationDoseDataBean> validVaccinations = ds.getDoseList();
+    
+    // count Trumenba and Bexsero doses
+    int numBexsero = countBexseroVaccinations(ds, validVaccinations);
+    int numTrumenba = countTrumenbaVaccinations(ds, validVaccinations);
+
+    ds.log(" + Currently examining " + numBexsero + " Bexsero dose(s)");
+    ds.log(" + Currently examining " + numTrumenba + " Trumenba dose(s)");
+
+    // there are exactly 2 Trumenba doses
+    ds.log("There are 2 doses of Trumenba");
+    if ( ! fourMonthsHavePastSinceSecondDoseOfTrumena(ds, validVaccinations) ) {
+      ds.log(" + There are 2 Trumenba doses and less than 4 months have passed since the 2nd dose.");
+      return "B2b";
+    }
+    else if ( ! sixMonthsHavePastSinceFirstDoseOfTrumena(ds, validVaccinations) ) {
+      ds.log(" + There are 2 Trumenba doses and less than 6 months have passed since the 1st dose.");
+      return "B2b";
+    }
+    else {
+      ds.log(" + There are 2 Trumenba doses and 4 months have passed since the 2nd dose and 6 months have passed since 1st dose.");
+      return "M-final";
+    }
+  }
+}
