227a1f5c0da6d72bdc1689eb48cd67d6210dec9d
Nathan <unknown>
2016-02-12 14:47:57 +0000

    Added Mening codes to TCH Forecaster for IHS

--- src/org/tch/forecast/core/SoftwareVersion.java
+++ src/org/tch/forecast/core/SoftwareVersion.java
@@ -8,8 +8,8 @@ public class SoftwareVersion
   // javap -constants -classpath tch-forecaster-test.jar org.tch.forecast.core.SoftwareVersion
   public static final String VERSION_MAJOR = "3";
   public static final String VERSION_MINOR = "12";
-  public static final String VERSION_PATCH = "02";
-  public static final String VERSION_RELEASE = "20160129";
+  public static final String VERSION_PATCH = "03";
+  public static final String VERSION_RELEASE = "20160212";
 
   public static final String VERSION = VERSION_MAJOR + "." + VERSION_MINOR + "." + VERSION_PATCH;
 
