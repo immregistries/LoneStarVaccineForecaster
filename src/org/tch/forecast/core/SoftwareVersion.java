6e57fb4f11fef0921f7713d97d9404dc2ecffea8
GordonC <unknown>
2016-10-26 18:49:42 +0000

    Update version to 3.14.04

--- src/org/tch/forecast/core/SoftwareVersion.java
+++ src/org/tch/forecast/core/SoftwareVersion.java
@@ -8,7 +8,7 @@ public class SoftwareVersion
   // javap -constants -classpath tch-forecaster-test.jar org.tch.forecast.core.SoftwareVersion
   public static final String VERSION_MAJOR = "3";
   public static final String VERSION_MINOR = "14";
-  public static final String VERSION_PATCH = "03";
+  public static final String VERSION_PATCH = "04";
   public static final String VERSION_RELEASE = "20160902";
 
   public static final String VERSION = VERSION_MAJOR + "." + VERSION_MINOR + "." + VERSION_PATCH;
