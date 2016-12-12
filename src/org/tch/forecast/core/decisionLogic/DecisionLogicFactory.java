7937e50c2653c8f6c43077d76d1bf4c26764175f
GordonC <unknown>
2016-12-12 23:07:32 +0000

    Add new DecisionLogic for HPV 2-dose state checking

--- src/org/tch/forecast/core/decisionLogic/DecisionLogicFactory.java
+++ src/org/tch/forecast/core/decisionLogic/DecisionLogicFactory.java
@@ -6,6 +6,7 @@ public class DecisionLogicFactory
   public static final String DL_FLU_2015 = "DL FLU 2015";
   public static final String DL_MENB_TWO_DOSE = "DL MenB TwoDose";
   public static final String DL_MENB_MULTI_DOSE = "DL MenB MultiDose";
+  public static final String DL_HPV_2ND_DOSE= "DL HPV 2nd";
   
   public static DecisionLogic getDecisionLogic(String name)
   {
@@ -25,6 +26,10 @@ public class DecisionLogicFactory
     else if (name.equalsIgnoreCase(DL_MENB_MULTI_DOSE))
     {
       return new MenBMultiDoseStateDecisionLogic();
+    }
+    else if (name.equalsIgnoreCase(DL_HPV_2ND_DOSE))
+    {
+      return new HpvSecondDoseDecisionLogic();
     }
     return dl;
   }
