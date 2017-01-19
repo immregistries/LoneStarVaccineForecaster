7b2edfbbee65c83cce8479646e148fc46e06c402
GordonC <unknown>
2017-01-19 17:44:35 +0000

    Updated HPV Schedule Updated version to 3.14.06

--- src/org/tch/forecast/core/SoftwareVersion.java
+++ src/org/tch/forecast/core/SoftwareVersion.java
@@ -8,8 +8,8 @@ public class SoftwareVersion
   // javap -constants -classpath tch-forecaster-test.jar org.tch.forecast.core.SoftwareVersion
   public static final String VERSION_MAJOR = "3";
   public static final String VERSION_MINOR = "14";
-  public static final String VERSION_PATCH = "05";
-  public static final String VERSION_RELEASE = "20161212";
+  public static final String VERSION_PATCH = "06";
+  public static final String VERSION_RELEASE = "20170119";
 
   public static final String VERSION = VERSION_MAJOR + "." + VERSION_MINOR + "." + VERSION_PATCH;
 
