95d1886e98927181a8db8dbca7df11c0771d3557
GordonC <unknown>
2017-06-08 20:45:23 +0000

    New MenB Decision Logic for 2017
    [changes originally made 2017-03-09]

--- src/org/tch/forecast/core/decisionLogic/DecisionLogicFactory.java
+++ src/org/tch/forecast/core/decisionLogic/DecisionLogicFactory.java
@@ -4,8 +4,10 @@ public class DecisionLogicFactory
 {
   public static final String DL_FLU_2014 = "DL FLU 2014";
   public static final String DL_FLU_2015 = "DL FLU 2015";
-  public static final String DL_MENB_TWO_DOSE = "DL MenB TwoDose";
-  public static final String DL_MENB_MULTI_DOSE = "DL MenB MultiDose";
+  public static final String DL_MENB_TWO_DOSE_A = "DL Trumenba A";
+  public static final String DL_MENB_TWO_DOSE_B = "DL Trumenba B";
+  public static final String DL_MENB_TWO_DOSE_C = "DL Trumenba C";
+//  public static final String DL_MENB_MULTI_DOSE = "DL MenB MultiDose";
 //  public static final String DL_HPV_2ND_DOSE_2016= "DL HPV 2nd";
   public static final String DL_HPV_2ND_DOSE_2017= "DL HPV 2-dose";
   public static final String DL_HPV_3RD_DOSE_2017= "DL HPV 3-dose";
@@ -21,13 +23,17 @@ public class DecisionLogicFactory
     {
       return new Flu2015DecisionLogic();
     }
-    else if (name.equalsIgnoreCase(DL_MENB_TWO_DOSE))
+    else if (name.equalsIgnoreCase(DL_MENB_TWO_DOSE_A))
     {
-      return new MenB2016TwoDoseStateDecisionLogic();
+      return new MenB2017TwoDoseStateDecisionLogic();
     }
-    else if (name.equalsIgnoreCase(DL_MENB_MULTI_DOSE))
+    else if (name.equalsIgnoreCase(DL_MENB_TWO_DOSE_B))
     {
-      return new MenB2016MultiDoseStateDecisionLogic();
+      return new MenB2017TwoDoseStateDecisionLogic();
+    }
+    else if (name.equalsIgnoreCase(DL_MENB_TWO_DOSE_C))
+    {
+      return new MenB2017TwoDoseStateDecisionLogic();
     }
     // Preserve HPV 2016 version
 //  else if (name.equalsIgnoreCase(DL_HPV_2ND_DOSE_2016))
