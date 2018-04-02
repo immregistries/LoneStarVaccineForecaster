b99e5e1bce9e64357bfe7bca508f7741147c789b
GordonC <unknown>
2018-04-02 20:10:06 +0000

    Start on 4.0.5 - Zoster update

--- src/org/tch/forecast/core/SoftwareVersion.java
+++ src/org/tch/forecast/core/SoftwareVersion.java
@@ -8,8 +8,8 @@ public class SoftwareVersion
   // javap -constants -classpath tch-forecaster-test.jar org.tch.forecast.core.SoftwareVersion
   public static final String VERSION_MAJOR = "4";
   public static final String VERSION_MINOR = "0";
-  public static final String VERSION_PATCH = "4";
-  public static final String VERSION_RELEASE = "20180305";
+  public static final String VERSION_PATCH = "5";
+  public static final String VERSION_RELEASE = "20180320";
 
   public static final String VERSION = VERSION_MAJOR + "." + VERSION_MINOR + "." + VERSION_PATCH;
 
