d2d09c6711002ca34f67f578e9baa8282df1a441
GordonC <unknown>
2018-05-23 04:58:30 +0000

    Update version to 4.0.8

--- src/org/tch/forecast/core/SoftwareVersion.java
+++ src/org/tch/forecast/core/SoftwareVersion.java
@@ -8,8 +8,8 @@ public class SoftwareVersion
   // javap -constants -classpath tch-forecaster-test.jar org.tch.forecast.core.SoftwareVersion
   public static final String VERSION_MAJOR = "4";
   public static final String VERSION_MINOR = "0";
-  public static final String VERSION_PATCH = "7";
-  public static final String VERSION_RELEASE = "20180428";
+  public static final String VERSION_PATCH = "8";
+  public static final String VERSION_RELEASE = "20180522";
 
   public static final String VERSION = VERSION_MAJOR + "." + VERSION_MINOR + "." + VERSION_PATCH;
 
