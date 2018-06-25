f106895d2205fe3756d01f462cc322024f64f6ec
GordonC <unknown>
2018-06-25 12:47:17 +0000

    Updated Zoster schedule and increment version to 4.1.0

--- src/org/tch/forecast/core/SoftwareVersion.java
+++ src/org/tch/forecast/core/SoftwareVersion.java
@@ -7,9 +7,9 @@ public class SoftwareVersion
   
   // javap -constants -classpath tch-forecaster-test.jar org.tch.forecast.core.SoftwareVersion
   public static final String VERSION_MAJOR = "4";
-  public static final String VERSION_MINOR = "0";
-  public static final String VERSION_PATCH = "9-SNAPSHOT";
-  public static final String VERSION_RELEASE = "20180522";
+  public static final String VERSION_MINOR = "1";
+  public static final String VERSION_PATCH = "0";
+  public static final String VERSION_RELEASE = "20180623";
 
   public static final String VERSION = VERSION_MAJOR + "." + VERSION_MINOR + "." + VERSION_PATCH;
 
