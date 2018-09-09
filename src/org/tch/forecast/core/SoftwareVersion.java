a8de16993935b5a5bf06047a207848ffe55cc8b6
GordonC <unknown>
2018-09-09 01:53:12 +0000

    [Note: previous commit made by Nathan and Scott.] Increment version number

--- src/org/tch/forecast/core/SoftwareVersion.java
+++ src/org/tch/forecast/core/SoftwareVersion.java
@@ -8,8 +8,8 @@ public class SoftwareVersion
   // javap -constants -classpath tch-forecaster-test.jar org.tch.forecast.core.SoftwareVersion
   public static final String VERSION_MAJOR = "4";
   public static final String VERSION_MINOR = "2";
-  public static final String VERSION_PATCH = "0";
-  public static final String VERSION_RELEASE = "20180824";
+  public static final String VERSION_PATCH = "1";
+  public static final String VERSION_RELEASE = "20180908";
 
   public static final String VERSION = VERSION_MAJOR + "." + VERSION_MINOR + "." + VERSION_PATCH;
 
